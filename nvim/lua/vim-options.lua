5bd37e3ea40f68f4f2a4fe1185ef1f97bcf29768
kamzhanyue <kamzhanyue@gmail.com>
2025-03-26 00:13:03 +1100

    refactor: add bufferline and buffer navigation commands

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,3 +4,9 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
+
+vim.keymap.set('n', 'tj', ':bprev<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'tk', ':bnext<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', 'tq', ':bd<CR>', { noremap = true, silent = true })
+
+
