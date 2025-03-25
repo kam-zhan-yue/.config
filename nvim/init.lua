9bfa5d256fb7585839cf841453b448bc7a9193c5
kamzhanyue <kamzhanyue@gmail.com>
2025-03-25 21:38:20 +1100

    refactor: add neo tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -28,10 +28,20 @@ vim.g.maplocalleader = "\\"
 local plugins = {
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
 	{
-    'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim' }
-  },
-  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+	    dependencies = { 'nvim-lua/plenary.nvim' },
+	},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+		},
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
 }
 
 local opts = {}
@@ -40,6 +50,7 @@ require("lazy").setup(plugins, opts)
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal right<CR>')
 
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
