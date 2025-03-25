ade5661b401f2f200562ccdb938d8a28203e9d3e
kamzhanyue <kamzhanyue@gmail.com>
2025-03-25 22:32:04 +1100

    refactor: setup lsps and language server dependenices

---
+++ nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,42 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "csharp_ls",
+          "biome",
+          "cssls",
+          "rust_analyzer",
+          "clangd",
+          "cmake",
+          "jinja_lsp",
+        }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      lspconfig.csharp_ls.setup({})
+      lspconfig.biome.setup({})
+      lspconfig.cssls.setup({})
+      lspconfig.rust_analyzer.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.cmake.setup({})
+      lspconfig.jinja_lsp.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
