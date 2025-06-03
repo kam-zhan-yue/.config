ffeb6b73b73011c447a08178e76340734b311a47
Alex Kam <kamzhanyue@gmail.com>
2025-06-03 20:47:39 +0900

    refactor: fix tab spacing

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -1,14 +1,12 @@
 vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set('n', 'tj', ':bprev<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'tk', ':bnext<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', 'tq', ':bd<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "tj", ":bprev<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "tk", ":bnext<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "tq", ":bd<CR>", { noremap = true, silent = true })
 
 vim.opt.relativenumber = true
-
-
