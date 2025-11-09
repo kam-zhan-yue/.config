15f8f525038819742cebf8c975008ff6545351ec
Alex Kam <kamzhanyue@gmail.com>
2025-11-09 16:12:32 +0900

    add harpoon

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -5,9 +5,9 @@ vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "tj", ":bprev<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "tk", ":bnext<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "tq", ":bd<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "tj", ":bprev<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "tk", ":bnext<CR>", { noremap = true, silent = true })
+-- vim.keymap.set("n", "tq", ":bd<CR>", { noremap = true, silent = true })
 
 -- vim.opt.relativenumber = true
 vim.opt.number = true
