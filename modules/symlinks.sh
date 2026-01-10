4194129b9b567c822d5464bd1e903a81b9580bc4
Alex Kam <kamzhanyue@gmail.com>
2026-01-10 22:21:33 +0900

    add .ideavimrc

--- modules/symlinks.sh
+++ modules/symlinks.sh
@@ -23,6 +23,9 @@ setup_symlink "${HOME}/.zshrc" "${HOME}/.config/zsh/.zshrc"
 # gitconfig
 setup_symlink "${HOME}/.gitconfig" "${HOME}/.config/git/.gitconfig"
 
+# ideavimrc
+setup_symlink "${HOME}/.ideavimrc" "${HOME}/.config/jetbrains/.ideavimrc"
+
 # TODO: Figure out how to symlink different gitconfigs depending on the machine you are on
 # In the meantime, uncomment this and run the command
 # setup_symlink "${HOME}/.gitconfig" "${HOME}/.config/git/.gitconfig.uptick"
